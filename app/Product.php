af7709298854979ae78bc61ab5b1cbd7f267bc7a
Marcos <markinhofacul@gmail.com>
2021-01-29 15:16:23 -0300

    atualizado laravel 7

--- app/Product.php
+++ app/Product.php
@@ -22,6 +22,11 @@ class Product extends Model
             ->saveSlugsTo('slug');
     }
 
+
+    public function getThumbAttribute(){
+      return  $this->photos->first()->image;
+    }
+
     public function store()
     {
         return $this->belongsTo(Store::class);
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Marketplace L6</title>
+    <title>Marketplace L6</title>   
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
@@ -72,46 +72,10 @@
     @yield('content')
 </div>
 
-<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-2.2.4.min.js" 
+    integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" 
+    crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 <script src="{{asset('js/app.js')}}"></script>
 @yield('scripts')
 </body>
-</html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        {{-- <ul class="navbar-nav mr-auto">
-            <li class="nav-item @if(request()->is('admin/stores*')) active @endif">
-                <a class="nav-link" href="{{route('admin.stores.index')}}">Lojas <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item @if(request()->is('admin/products*')) active @endif">
-                <a class="nav-link" href="{{route('admin.products.index')}}">Produtos</a>
-            </li>
-            <li class="nav-item @if(request()->is('admin/categories*')) active @endif">
-                <a class="nav-link" href="{{route('admin.categories.index')}}">Categorias</a>
-            </li>
-        </ul> --}}
